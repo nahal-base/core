66d50c58dd414af8072886b5f6adac1858ed3731
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-12 15:26:22 +0330

    :sparkles: add regex

--- composable/regex.ts
+++ composable/regex.ts
@@ -1,8 +1,11 @@
 export const useRegex = () => {
-  const email = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gim
-  const password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&_])[A-Za-z\d$@$!%*?&_]{6,40}$/
-  const rtl = /^[\u0600-\u06FF\s]+$/
-  const ltr = /^[a-zA-Z0-9?><;,{}[\]\-_+=!@#$%&*|' ]*$/
+  const email = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gim;
+  const password =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&_])[A-Za-z\d$@$!%*?&_]{6,40}$/;
+  const rtl = /^[\u0600-\u06FF\s]+$/;
+  const ltr = /^[a-zA-Z0-9?><;,{}[\]\-_+=!@#$%&*|' ]*$/;
+  const phone = /^0\d{2,3}-\d{8}$/;
+  const mobile = /((0?9)|(\+?989))\d{9}/g;
 
-  return { email, password, rtl, ltr }
-}
+  return { email, password, rtl, ltr, phone, mobile };
+};
