ad232b02bfef0d7ac16ae6fe5e8814d190de27ba
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-13 13:53:54 +0330

    :sparkles: add more rule and regex

--- composable/rules.ts
+++ composable/rules.ts
@@ -2,10 +2,18 @@ import { useI18n } from 'vue-i18n'
 import { useRegex } from './regex'
 export const useRules = () => {
   const { t } = useI18n()
-  const regex = useRegex()
+   const regex = useRegex()
   const password = { required: true, message: t('rules.password'), pattern: regex.password }
   const ltr = { required: true, message: t('rules.ltr'), pattern: regex.ltr }
   const email = { required: true, message: t('rules.email'), pattern: regex.email }
-  const required = { required: true, message: t('rules.required') }
-  return { required, email, password, ltr }
+  const required = { required: true, message: t("rules.required") };
+  const number = { required: true, message: t('rules.number'), pattern: regex.number }
+  const initNumber = { required: true, message: t('rules.initNumber'), pattern: regex.initNumber }
+  const url = { required: true, message: t("rules.url"), pattern: regex.url };
+  const ipv4 = { required: true, message: t("rules.ipv4"), pattern: regex.ipv4 };
+  const mobile = { required: true, message: t("rules.mobile"), pattern: regex.mobile };
+  const phone = { required: true, message: t("rules.phone"), pattern: regex.phone };
+  const nationalCode = { required: true, message: t("rules.nationalCode"), pattern: regex.nationalCode };
+
+  return { required, email, password, ltr, initNumber, number, url,ipv4,mobile,phone,nationalCode };
 }
