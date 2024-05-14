17f92baadedddb8edc807ded856b695408e88aff
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-14 08:57:31 +0330

    :sparkles: add more i18n

--- i18n/en/index.ts
+++ i18n/en/index.ts
@@ -1,20 +1,21 @@
 export default {
   rules: {
-    required: 'This field is required',
-    email: 'Please enter a valid email',
-    password: 'Password must be between 6 and 40 characters'
+    required: "This field is required",
+    email: "Please enter a valid email",
+    password: "Password must be between 6 and 40 characters",
+    initNumber: "Please enter an initial number",
   },
 
   form: {
-    username: 'Username',
-    password: 'Password',
-    login: 'Login',
-    forget: 'Forget password'
+    username: "Username",
+    password: "Password",
+    login: "Login",
+    forget: "Forget password",
   },
 
   panel: {
-    color: 'Panel color',
-    exit: 'Exit',
-    language: 'Panel language'
-  }
-}
+    color: "Panel color",
+    exit: "Exit",
+    language: "Panel language",
+  },
+};
