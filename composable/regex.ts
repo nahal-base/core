9ca78b2efa344a4acbf57a9e3eab5e0a7c314c9d
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-14 09:20:15 +0330

    :bug: fix urlOrIP

--- composable/regex.ts
+++ composable/regex.ts
@@ -11,7 +11,8 @@ export const useRegex = () => {
   const number = /^\d+$/;
   const initNumber = /^[0-9]+$/
   const floatNumber = /^-?\d+(\.\d+)?$/
-  const urlAndIIpRegex = /^(?:(?:https?|ftp):\/\/)?(?:[\w-]+\.)+[a-z]{2,6}(?::\d{1,5})?(?:\/\S*)?$/
+  const ipv4OrUrl = /^(?:(?:https?|ftp):)?\/\/(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
 
   return {
     email,
@@ -26,6 +27,6 @@ export const useRegex = () => {
     number,
     initNumber,
     floatNumber,
-    urlAndIIpRegex,
+    ipv4OrUrl,
   };
 }
