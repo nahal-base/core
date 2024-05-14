89dd05c784fea95e02f318a1d24c017cef06370f
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-14 07:59:18 +0330

    :sparkles: add more function for useModal

--- composable/modal.ts
+++ composable/modal.ts
@@ -1,23 +1,59 @@
-import { computed, ref } from 'vue'
-import { message } from 'ant-design-vue/es'
-import { useI18n } from 'vue-i18n'
-import { isFunction } from 'lodash'
+import { computed, ref } from "vue";
+import { message } from "ant-design-vue/es";
+import { useI18n } from "vue-i18n";
+import { isFunction } from "lodash";
 
 export const useModal = () => {
-  const isOpenRef = ref<boolean>(false)
+  const isOpen = ref<boolean>(false);
 
-  const isOpen = computed(() => isOpenRef.value)
+  // const isOpen = computed(() => isOpenRef.value);
 
   const open = (callBack: Function) => {
     if (isFunction(callBack)) {
-      callBack()
+      callBack();
     }
-    isOpenRef.value = true
-  }
+    isOpen.value = true;
+  };
   const close = () => {
-    isOpenRef.value = false
-  }
+    isOpen.value = false;
+  };
 
-  return { isOpen, open, close }
-}
+  const toggle = () => {
+    isOpen.value = !isOpen.value;
+  };
+  const invokeToggle = (callback: Function) => {
+    if (isFunction(callback)) {
+      callback();
+    }
+    isOpen.value = !isOpen.value;
+  };
+  const invokeAsyncToggle = async (callback: Function) => {
+    if (isFunction(callback)) {
+      await callback();
+    }
+    isOpen.value = !isOpen.value;
+  };
+  const toggleInvoke = (callback: Function) => {
+    isOpen.value = !isOpen.value;
+    if (isFunction(callback)) {
+      callback();
+    }
+  };
+  const toggleInvokeAsync = async (callback: Function) => {
+    isOpen.value = !isOpen.value;
+    if (isFunction(callback)) {
+      await callback();
+    }
+  };
 
+  return {
+    isOpen,
+    open,
+    close,
+    toggle,
+    invokeToggle,
+    toggleInvoke,
+    invokeAsyncToggle,
+    toggleInvokeAsync,
+  };
+};
