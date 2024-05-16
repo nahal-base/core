4eeb187f53e79d733b4fa7ae87bfe61874fbf6f2
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-16 10:15:48 +0330

    :sparkles: add more tanslates

--- i18n/en/index.ts
+++ i18n/en/index.ts
@@ -4,14 +4,15 @@ export default {
     email: "Email is not entered correctly",
     password: "Password must be between 6 and 40 characters",
     ltr: "Only English characters",
-    initNumber: "The number should not be negative",
+    initNumber: "Number should not be negative",
     number: "This field must be a number",
     url: "Please enter a valid URL address",
     ipv4: "Please enter a valid IPv4 address",
     mobile: "Please enter a valid mobile number",
     phone: "Please enter a valid phone number",
     ipv4OrUrl: "Please enter a valid IP address or URL",
-    value: "Unique identifier",
+    value: "Please enter a unique identifier",
+    nationalCode: "Please enter a valid national code",
   },
 
   form: {
