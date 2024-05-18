9cdfecfa104c4af149a808d1ae1a3cb5f4e294b5
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-18 16:43:16 +0330

    :lipstick: add css animation

--- layouts/PanelLayout/PanelMain.vue
+++ layouts/PanelLayout/PanelMain.vue
@@ -1,9 +1,22 @@
 <template>
   <LayoutContent class="overflow-y-auto p-6">
-    <RouterView />
+    <Transition name="fade">
+      <RouterView />
+    </Transition>
   </LayoutContent>
 </template>
 <script lang="ts" setup>
 import { RouterView } from 'vue-router'
 import { LayoutContent } from 'ant-design-vue/es'
 </script>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .5s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
