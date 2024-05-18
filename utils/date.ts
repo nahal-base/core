f0beaa613e538b614fb23a7a10acaed7fd409d06
Amir Maghami <Maghami.A1987@gmail.com>
2024-05-18 11:08:22 +0330

    :sparkles: add custom format for date

--- utils/date.ts
+++ utils/date.ts
@@ -2,18 +2,18 @@ import { useConfigStore } from '@/core/stores/configStore'
 import dayjs from 'dayjs'
 import { isDate } from 'lodash'
 
-export const formatDate = (_input: any) => {
-  const configStore = useConfigStore()
-  const dateObject = new Date(_input)
+export const formatDate = (_input: any, format?: string) => {
+  const configStore = useConfigStore();
+  const dateObject = new Date(_input);
 
   if (isDate(dateObject) && _input) {
-    if (configStore.locale.locale === 'en') {
-      return dayjs(_input).format('YYYY/MM/DD - HH:mm')
-    } else if (configStore.locale.locale === 'fa') {
-      dayjs.calendar('jalali')
-      return dayjs(_input).format('HH:mm - YYYY/MM/DD')
+    if (configStore.locale.locale === "en") {
+      return dayjs(_input).format(format ?? "YYYY/MM/DD - HH:mm");
+    } else if (configStore.locale.locale === "fa") {
+      dayjs.calendar("jalali");
+      return dayjs(_input).format(format ?? "HH:mm - YYYY/MM/DD");
     }
   } else {
-    return '-'
+    return "-";
   }
-}
+};
